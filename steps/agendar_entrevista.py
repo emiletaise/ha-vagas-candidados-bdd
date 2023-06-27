34cb3b1c5c6bb19ca74fe7c31b1b7d44ea6a221b
Emile Taíse <53312088+emiletaise@users.noreply.github.com>
2023-06-27 20:21:43 -0300

    Add files via upload

--- steps/agendar_entrevista.py
+++ steps/agendar_entrevista.py
@@ -1,18 +1,16 @@
 from behave import given, when, then
-from candidatos import * 
+from candidatos import *
 
 @given('existem candidatos aptos')
-def existe_candidatos_apto(context):
-    context.candidatos_apto = candidato_reconhecido_previamente()
+def existem_candidatos_aptos(context):
+    context.candidatos_aptos = candidato_reconhecido_previamente  # Implemente a função para carregar os candidatos aptos
 
 @when('agendo a entrevista')
 def agendo_entrevista(context):
-    entrevista_generator = agendar_entrevista(context.ambiente_de_simulacao, context.candidatos_apto)
-    context.entrevista_iterator = iter(entrevista_generator)
+    entrevista_agendada = agendar_entrevista(context.candidatos_aptos())  # Implemente a função para agendar a entrevista
+    context.entrevista_agendada = entrevista_agendada
 
 @then('a entrevista é agendada para uma vaga compatível')
-def entrevista_agendada(context):
-    try:
-        next(context.entrevista_iterator)
-    except StopIteration:
-        pass
+def entrevista_agendada_para_vaga_compativel(context):
+    assert context.entrevista_agendada is not None, "A entrevista não foi agendada"
+    assert context.entrevista_agendada.vaga.compativel, "A entrevista foi agendada para uma vaga incompatível"
